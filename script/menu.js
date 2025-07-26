f9392877cf0ff84a826ce987d598703693fe81c2
Harnish <harrnish@gmail.com>
2025-07-26 09:26:51 -0400

    updated menu animation

--- script/menu.js
+++ script/menu.js
@@ -17,6 +17,34 @@ let lastScrollY = window.scrollY;
 let isMenuVisible = true;
 let isAnimating = false;
 let splitTexts = [];
+let footerSplitTexts = [];
+
+function scrambleText(elements, duration = 0.4) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+  elements.forEach((char, index) => {
+    const originalText = char.textContent;
+    let iterations = 0;
+    const maxIterations = Math.floor(Math.random() * 6) + 3;
+
+    gsap.set(char, { opacity: 1 });
+
+    const scrambleInterval = setInterval(() => {
+      char.textContent = chars[Math.floor(Math.random() * chars.length)];
+      iterations++;
+
+      if (iterations >= maxIterations) {
+        clearInterval(scrambleInterval);
+        char.textContent = originalText;
+      }
+    }, 25);
+
+    setTimeout(() => {
+      clearInterval(scrambleInterval);
+      char.textContent = originalText;
+    }, duration * 1000);
+  });
+}
 
 function initMenu() {
   gsap.set(menuOverlay, {
@@ -39,12 +67,30 @@ function initMenu() {
     }
   });
 
+  const footerElements = document.querySelectorAll(
+    ".menu-social a, .menu-social span, .menu-time"
+  );
+  footerElements.forEach((element) => {
+    const split = new SplitText(element, {
+      type: "chars",
+    });
+    footerSplitTexts.push(split);
+
+    gsap.set(split.chars, {
+      opacity: 0,
+    });
+
+    if (element.classList.contains("menu-time")) {
+      gsap.set(element, { opacity: 0 });
+    }
+  });
+
   gsap.set(menuItems, {
     opacity: 1,
   });
 
   gsap.set(menuFooter, {
-    opacity: 0,
+    opacity: 1,
     y: 20,
   });
 }
@@ -100,11 +146,26 @@ function openMenu() {
     menuFooter,
     {
       duration: 0.3,
-      opacity: 1,
       y: 0,
       ease: "power2.out",
+      onComplete: () => {
+        const timeElement = document.querySelector(".menu-time");
+        if (timeElement) {
+          gsap.set(timeElement, { opacity: 1 });
+        }
+
+        const allFooterChars = footerSplitTexts.reduce((acc, split) => {
+          return acc.concat(split.chars);
+        }, []);
+
+        allFooterChars.forEach((char, index) => {
+          setTimeout(() => {
+            scrambleText([char], 0.4);
+          }, index * 30);
+        });
+      },
     },
-    "-=0.3"
+    "-=1"
   );
 }
 
@@ -130,9 +191,19 @@ function closeMenu() {
 
   tl.to([menuFooter], {
     duration: 0.3,
-    opacity: 0,
     y: 20,
     ease: "power2.in",
+    onStart: () => {
+      const timeElement = document.querySelector(".menu-time");
+      if (timeElement) {
+        gsap.set(timeElement, { opacity: 0 });
+      }
+
+      const allFooterChars = footerSplitTexts.reduce((acc, split) => {
+        return acc.concat(split.chars);
+      }, []);
+      gsap.set(allFooterChars, { opacity: 0 });
+    },
   });
 
   tl.to(
@@ -185,7 +256,24 @@ function updateTime() {
   });
   const timeElement = document.querySelector(".menu-time");
   if (timeElement) {
-    timeElement.textContent = `${timeString} LOCAL`;
+    if (!isOpen) {
+      timeElement.textContent = `${timeString} LOCAL`;
+    } else {
+      const timeSplit = footerSplitTexts.find(
+        (split) => split.element === timeElement
+      );
+
+      if (timeSplit && timeSplit.chars) {
+        const newText = `${timeString} LOCAL`;
+        const oldChars = timeSplit.chars;
+
+        newText.split("").forEach((char, index) => {
+          if (oldChars[index]) {
+            oldChars[index].textContent = char;
+          }
+        });
+      }
+    }
   }
 }
 
@@ -211,14 +299,6 @@ function init() {
 
   updateTime();
   setInterval(updateTime, 1000);
-}
-
-// Cleanup function to revert SplitText when needed
-function cleanup() {
-  splitTexts.forEach((split) => {
-    split.revert();
-  });
-  splitTexts = [];
 }
 
 if (document.readyState === "loading") {
