ead9147b01b889c32648ca65d88a644e48b9327d
Harnish <harrnish@gmail.com>
2025-07-26 11:40:36 -0400

    fine tuning 5

--- script/lenis-scroll.js
+++ script/lenis-scroll.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const scrollSettings = isMobile
     ? {
-        duration: 1,
+        duration: 0.8,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         direction: "vertical",
         gestureDirection: "vertical",
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
         smoothTouch: true,
         touchMultiplier: 1.5,
         infinite: false,
-        lerp: 0.05,
+        lerp: 0.09,
         wheelMultiplier: 1,
         orientation: "vertical",
         smoothWheel: true,
